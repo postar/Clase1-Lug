a1852ef08eb80a009d07cc84bac1156d4ed33c31
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2022-08-20 18:56:57 -0300

    FirstCommit

---
+++ Clase1-Lug/Acceso.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Clase1_Lug
+{
+    public class Acceso
+    {
+        private SqlConnection conexion;
+        public void Abrir()
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = "Initial Catalog=Base; Data Source=.SQLEXPRESS; Integrated Security=SSPI; ";
+            conexion.Open();
+        }
+        public void Cerrar()
+        {
+            conexion.Close();
+            conexion = null;
+            GC.Collect();
+        }
+        public void Escribir (string sql)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = sql;
+            try
+            {
+                comando.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+            }
+        }
+        public SqlDataReader Leer(string sql)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = sql;
+            return comando.ExecuteReader();
+        }
+    }
+}
---
+++ Clase1-Lug/Empleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clase1_Lug
+{
+    public class Empleado
+    {
+
+    }
+}
